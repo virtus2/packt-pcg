4077c790b0125ad89b936a35e026e4c4a1c3b533
Kim Hyun Deok <khd1323@naver.com>
2024-03-14 00:31:54 +0900

    3장 끝없는 세계의 생성 - end

--- Assets/_Complete-Game/Scripts/GameManager.cs
+++ Assets/_Complete-Game/Scripts/GameManager.cs
@@ -1,69 +1,112 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.Collections;
 
-namespace Completed
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Completed;
+
+public class GameManager : MonoBehaviour
 {
-	using System.Collections.Generic;		//Allows us to use Lists. 
-	using UnityEngine.UI;					//Allows us to use UI.
-	
-	public class GameManager : MonoBehaviour
-	{
-		public float turnDelay = 0.1f;							//Delay between each Player turn.
-		public int healthPoints = 100;							//Starting value for Player health points.
-		public static GameManager instance = null;				//Static instance of GameManager which allows it to be accessed by any other script.
-		[HideInInspector] public bool playersTurn = true;       //Boolean to check if it's players turn, hidden in inspector but public.
+    public float turnDelay = 0.1f;                          //Delay between each Player turn.
+    public int healthPoints = 100;                          //Starting value for Player health points.
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+    [HideInInspector] public bool playersTurn = true;       //Boolean to check if it's players turn, hidden in inspector but public.
 
-        private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
-        private List<Enemy> enemies;                            //List of all Enemy units, used to issue them move commands.
-        private bool enemiesMoving;								//Boolean to check if enemies are moving.
+    //CHAPTER 3
+    private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
+    private List<Enemy> enemies;                            //List of all Enemy units, used to issue them move commands.
+    private bool enemiesMoving;                             //Boolean to check if enemies are moving.
 
-        private Text levelText;									//Text to display current level number.
-		private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
-		private int level = 1;									//Current level number, expressed in game as "Day 1".
-		private bool doingSetup = true;                         //Boolean to check if we're setting up board, prevent Player from moving during setup.
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
 
-		//Awake is always called before any Start functions
-		void Awake()
-		{
-			//Check if instance already exists
-			if (instance == null)
-			{
-				//if not, set instance to this
-				instance = this;
-			}
+            //if not, set instance to this
+            instance = this;
 
-			//If instance already exists and it's not this:
-			else if (instance != this)
-			{
-				//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-				Destroy(gameObject);
-            }
+        //If instance already exists and it's not this:
+        else if (instance != this)
 
-            //Sets this to not be destroyed when reloading scene
-            DontDestroyOnLoad(gameObject);
-			
-			//Assign enemies to a new List of Enemy objects.
-			enemies = new List<Enemy>();
-			
-			//Get a component reference to the attached BoardManager script
-			boardScript = GetComponent<BoardManager>();
-			
-			//Call the InitGame function to initialize the first level 
-			InitGame();
-		}
-		
-		//Initializes the game for each level.
-		void InitGame()
-		{
-			enemies.Clear();
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
 
-			boardScript.BoardSetup();
-		}
-		
-		public void UpdateBoard(int horizontal, int vertical)
-		{
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Assign enemies to a new List of Enemy objects.
+        enemies = new List<Enemy>();
+
+        //CHAPTER 3
+        //Get a component reference to the attached BoardManager script
+        boardScript = GetComponent<BoardManager>();
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+
+    //This is called each time a scene is loaded.
+    void OnLevelWasLoaded(int index)
+    {
+        //Call InitGame to initialize our level.
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+        //Clear any Enemy objects in our List to prepare for next level.
+        enemies.Clear();
+
+        //Call the SetupScene function of the BoardManager script, pass it current level number.
+        boardScript.BoardSetup();
+    }
+
+    //Update is called every frame.
+    void Update()
+    {
+        //Check that playersTurn or enemiesMoving or doingSetup are not currently true.
+        if (playersTurn || enemiesMoving)
+
+            //If any of these are true, return and do not start MoveEnemies.
+            return;
+
+        //Start moving enemies.
+        StartCoroutine(MoveEnemies());
+    }
+
+    //GameOver is called when the player reaches 0 health points
+    public void GameOver()
+    {
+        //Disable this GameManager.
+        enabled = false;
+    }
+
+    //Coroutine to move enemies in sequence.
+    IEnumerator MoveEnemies()
+    {
+        //While enemiesMoving is true player is unable to move.
+        enemiesMoving = true;
+
+        //Wait for turnDelay seconds, defaults to .1 (100 ms).
+        yield return new WaitForSeconds(turnDelay);
+
+        //If there are no enemies spawned (IE in first level):
+        if (enemies.Count == 0)
+        {
+            //Wait for turnDelay seconds between moves, replaces delay caused by enemies moving when there are none.
+            yield return new WaitForSeconds(turnDelay);
         }
-	}
-}
 
+        playersTurn = true;
+
+        //Enemies are done moving, set enemiesMoving to false.
+        enemiesMoving = false;
+    }
+
+    public void updateBoard(int horizantal, int vertical)
+    {
+        boardScript.addToBoard(horizantal, vertical);
+    }
+}
